30541307999e0808aba28290232ccbf6b0b8dcb1
nboylan-tandq <neal.boylan@klasgroup.com>
2024-10-10 16:54:47 +0100

    Added spinner to select workout and made more gym specific inputs

--- app/src/main/java/org/wit/gym_android_app/activities/GymActivity.kt
+++ app/src/main/java/org/wit/gym_android_app/activities/GymActivity.kt
@@ -3,6 +3,7 @@ package org.wit.gym_android_app.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import org.wit.gym_android_app.R
 import org.wit.gym_android_app.databinding.ActivityGymBinding
 import org.wit.gym_android_app.models.WorkoutModel
 import timber.log.Timber
@@ -23,9 +24,19 @@ class GymActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
         i("Gym Activity started..")
 
+        var workoutKeys = resources.getStringArray(R.array.weeklyWorkoutKeys)
+        var workoutVals: Array<String> = resources.getStringArray(R.array.weeklyWorkouts)
+        var workoutMap = mutableMapOf<String, String>()
+        for (i in workoutKeys.indices) {
+            i("workout keys: ${workoutKeys[i]}")
+            workoutMap[workoutKeys[i]] = workoutVals[i]
+        }
+
         binding.btnAdd.setOnClickListener() {
-            workout.title = binding.workoutTitle.text.toString()
-            workout.description = binding.workoutDescription.text.toString()
+            workout.title = binding.spinner.selectedItem.toString()
+            binding.workoutExercise.setText(workoutMap[binding.spinner.selectedItem.toString()])
+            workout.weight = binding.workoutWeight.text.toString()
+
             if (workout.title.isNotEmpty()) {
                 i("add Button Pressed: $workout.title")
                 workouts.add(workout.copy())
--- app/src/main/java/org/wit/gym_android_app/models/WorkoutModel.kt
+++ app/src/main/java/org/wit/gym_android_app/models/WorkoutModel.kt
@@ -1,4 +1,5 @@
 package org.wit.gym_android_app.models
 
 data class WorkoutModel(var title: String = "",
-                        var description: String = "")
+                        var exercise: String = "",
+                        var weight: String ="")
